cf1909b88c5c48b7f425f599e741d3bf2918938b
Hokydarknes <144889189+Hokydarknes@users.noreply.github.com>
2023-10-30 21:41:23 +0300

    увеличивает положительные числа а отрицательные меня на противоположное
    увеличивает положительные числа а отрицательные меня на противоположное

---
+++ увелечение и замена.cpp
@@ -0,0 +1,21 @@
+﻿// увелечение и замена.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<math.h>
+using namespace std;
+void pomni(float &x){
+	x=(x >= 0)? 2 * x : -x;
+	
+}
+
+
+int main()
+{
+	float x;
+	cout << "enter number" << endl;
+	cin >> x;
+	pomni(x);
+	(x >= 0) ? cout<< 2 * x:cout << -x;
+	return 0;
+}
