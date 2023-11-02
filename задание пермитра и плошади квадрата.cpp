b8f3f79b37ee9044bcfad3d7a1a4e4a590875f36
Hokydarknes <144889189+Hokydarknes@users.noreply.github.com>
2023-11-02 22:33:20 +0300

    Add files via upload
    задание пермитра и плошади квадрата.cpp

---
+++ задание пермитра и плошади квадрата.cpp
@@ -0,0 +1,33 @@
+﻿// задание пермитра и плошади квадрата.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+void kva(float side,float& perimeter,float& square){                // обьявление функцие
+    perimeter = 4 * side;
+    square = side * side;
+}
+
+int main()
+{
+    float side;                                                     //присвоение вывода
+    cout << "enter the data" << endl;
+    cin >> side;
+    float square, perimeter;
+    kva(side, perimeter, square);
+    cout << " perimeter kva " << perimeter << endl;
+    cout << " square kva " << square << endl;
+    if (perimeter > square) {                               // выполняется проверка 
+        cout << "perimeter more" << endl;
+    
+    }
+    else if (square > perimeter) {
+        cout << "square more" << endl;
+    }
+    else {
+        cout << "=" << endl;
+    }
+    return 0;
+}
+
